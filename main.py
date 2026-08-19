8f90617b1f43b7a0885d030480834b7d9256c644
Vijay Panchal <vjy.prs@gmail.com>
2026-08-20 00:08:25 +0530

    Coversation done

--- main.py
+++ main.py
@@ -10,29 +10,36 @@ client = Anthropic()
 model = "claude-haiku-4-5"
 
 
+def add_user_message(messages, content):
+    user_message = {"role": "user", "content": content}
+    messages.append(user_message)
+    return messages
+
+def add_assistant_message(messages, content):
+    assistant_message = {"role": "assistant", "content": content}
+    messages.append(assistant_message)
+    return messages 
+
+def chat(messages):
+    response = client.messages.create(
+        model=model,
+        max_tokens=100,
+        messages=messages,
+    )
+    return response.content[0].text
+
 def main():
 
-    respose = client.messages.create(
-        model=model,
-        max_tokens=100,
-        messages=[
-            {
-                "role": "user",
-                "content": "Write is quantum computing ? Answer in one line",
-            }
-        ],
-    )
+    messages = []
+    add_user_message(messages, "Define Quantum Computing in one sentence.")
+    answer = chat(messages)
+    add_assistant_message(messages, answer)
 
-    pprint(respose.content[0].text)
+    add_user_message(messages, "Write anouther sentence")
+    answer2 = chat(messages)
+    add_assistant_message(messages, answer2)
 
-    respose2 = client.messages.create(
-        model=model,
-        max_tokens=100,
-        messages=[{"role": "user", "content": "Write one more line"}],
-    )
-
-    pprint(respose2.content[0].text)
-
+    pprint(messages)
 
 if __name__ == "__main__":
     main()
